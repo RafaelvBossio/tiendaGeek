a7a6fe08247ba8bbb39780b3b4dba83607c1dea8
Rafael Alberto Vargas Bossio <rafaelvargasbossio99@gmail.com>
2022-05-08 17:41:42 -0500

    optimizacion del codigo mostrarCategoria
    Para mejorar el entendimiento del código

--- src/js/mostrarCategoria.js
+++ src/js/mostrarCategoria.js
@@ -4,69 +4,27 @@ const starWars = document.querySelector("[data-starWars]");
 const consolas = document.querySelector("[data-consolas]");
 const diversos = document.querySelector("[data-diversos]");
 
-let starwar = 0;
-let consola = 0;
-let diverso = 0;
-
-productosServicios.listaProductos().then((data) => {
-  data.forEach((element) => {
-    if (element.categoria == "starWars" && starwar < 6) {
+function mostrarProductos(array, categoria, cantidad, ul) {
+  let bandera = 0;
+  
+  array.forEach((element) => {
+    if (element.categoria == categoria && bandera < cantidad) {
       for (var i = 0; i < 1; i++) {
         const li = document.createElement("li");
-        li.classList.add("producto")
+        li.classList.add("producto");
 
-        const contenido = `<div class="imagen-producto">
-              <a id="${element.id}" href="./src/pages/producto.html?id=${element.id}?catg=${element.categoria}">
-                <img src="${element.imagen}" alt="imagen de producto" />
-              </a>
-              </div>
-              <div class="info-producto">
-                <h4 class="nombre-producto">${element.nombre}</h4>
-                <p class="precio-producto">${element.precio}</p>
-              </div>`;
+        const contenido = `<div class="imagen-producto"><a id="${element.id}" href="./src/pages/producto.html?id=${element.id}?catg=${element.categoria}"><img src="${element.imagen}" alt="imagen de producto" /></a></div><div class="info-producto"><h4 class="nombre-producto">${element.nombre}</h4><p class="precio-producto">${element.precio}</p></div>`;
 
         li.innerHTML = contenido;
-        starWars.appendChild(li);
-        starwar++;
+        ul.appendChild(li);
+        bandera++;
       }
-    } else if(element.categoria == "consola" && consola < 6){
-        for (var i = 0; i < 1; i++) {
-            const li = document.createElement("li");
-            li.classList.add("producto")
-    
-            const contenido = `<div class="imagen-producto">
-                  <a id="${element.id}" href="./src/pages/producto.html?id=${element.id}?catg=${element.categoria}">
-                    <img src="${element.imagen}" alt="imagen de producto" />
-                  </a>
-                  </div>
-                  <div class="info-producto">
-                    <h4 class="nombre-producto">${element.nombre}</h4>
-                    <p class="precio-producto">${element.precio}</p>
-                  </div>`;
-    
-            li.innerHTML = contenido;
-            consolas.appendChild(li);
-            consola++;
-          }
-    } else if(element.categoria == "otro" && diverso < 6){
-        for (var i = 0; i < 1; i++) {
-            const li = document.createElement("li");
-            li.classList.add("producto")
-    
-            const contenido = `<div class="imagen-producto">
-                  <a id="${element.id}" href="./src/pages/producto.html?id=${element.id}?catg=${element.categoria}">
-                    <img src="${element.imagen}" alt="imagen de producto" />
-                  </a>
-                  </div>
-                  <div class="info-producto">
-                    <h4 class="nombre-producto">${element.nombre}</h4>
-                    <p class="precio-producto">${element.precio}</p>
-                  </div>`;
-    
-            li.innerHTML = contenido;
-            diversos.appendChild(li);
-            diverso++;
-          }
     }
   });
+}
+
+productosServicios.listaProductos().then((data) => {
+  mostrarProductos(data, "starWars", 6, starWars);
+  mostrarProductos(data, "consola", 6, consolas);
+  mostrarProductos(data, "otro", 6, diversos);
 });
